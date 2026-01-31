897dce6093f859ce3091221ee12d1de5c5c23c35
sfrabby <fazlerabby6732@gmail.com>
2026-01-31 11:41:20 +0600

    artical page controller added

--- lib/Config/AppColors.dart
+++ lib/Config/AppColors.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 import 'package:hexcolor/hexcolor.dart';
 
-final Color MainColor = HexColor("##415a77    .");
+final Color MainColor = HexColor("##415a77");
 final Color SeconderyColor = HexColor("#778da9");
 final Color White = HexColor("#F9FAFB");
---
+++ lib/Controller/Artical Page Controller.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app_with_getx/model/News%20Model.dart';
+
+class NewsController extends GetxController {
+  RxBool isLoading = false.obs;
+
+  RxList<Articles> trNews = <Articles>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getNews();
+  }
+
+  Future<void> getNews() async {
+    try {
+      isLoading.value = true;
+      trNews.clear();
+
+      var url = Uri.parse(
+        "https://newsapi.org/v2/everything?q=apple&from=2026-01-28&to=2026-01-28&sortBy=popularity&apiKey=56c0d5d277014ea5877ca61e1e131432",
+      );
+
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var body = jsonDecode(response.body);
+
+        NewsModel res = NewsModel.fromJson(body);
+
+        if (res.articles != null) {
+          trNews.assignAll(res.articles as Iterable<Articles>);
+        }
+      } else {
+        log("Server Error: ${response.statusCode}");
+      }
+    } catch (e) {
+      log("Exception: $e");
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
